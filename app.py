6355d9689d9daa43c85cf2f6dc8292d9ac3384ec
Alexandr Xoxon <4218820@gmail.com>
2024-09-16 22:56:20 +0300

    Automatic commit on Mon Sep 16 22:56:20 MSK 2024

--- app.py
+++ app.py
@@ -1,27 +1,46 @@
 import os
 import requests
+
+
+import requests_cache
+import pandas as pd
+from datetime import datetime
+from openmeteo_requests import Client
 import openmeteo_requests
 
 import requests_cache
 import pandas as pd
 from retry_requests import retry
 
+from tabulate import tabulate
+
 from dotenv import load_dotenv
 
 load_dotenv()
+
+
+def format_numbers(df, column_width=2):
+    """Форматирует числа с одинаковым количеством отступов."""
+    df = df.copy()  # Создаем копию DataFrame
+    for col in df.select_dtypes(include=['number']).columns:
+        df[col] = df[col].apply(
+            lambda x: f"{x:>{column_width}.3f}")  # Форматируем числа
+    return df
 
 
 def get_weather():
     # Setup the Open-Meteo API client with cache and retry on error
     cache_session = requests_cache.CachedSession('.cache', expire_after=3600)
     retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
-    openmeteo = openmeteo_requests.Client(session=retry_session)
+    openmeteo = Client(session=retry_session)
 
     url = "https://api.open-meteo.com/v1/forecast"
     params = {
-        "latitude": 52.52,
-        "longitude": 13.41,
+        "latitude": 47.2227,
+        "longitude": 39.7165,
         "hourly": "temperature_2m",
+        "daily": "weather_code",
+        "timezone": "Europe/Moscow",
         "forecast_days": 1
     }
     responses = openmeteo.weather_api(url, params=params)
@@ -35,16 +54,37 @@ def get_weather():
     hourly = response.Hourly()
     hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
 
-    hourly_data = {"date": pd.date_range(
+    hourly_data = {"Дата": pd.date_range(
         start=pd.to_datetime(hourly.Time(), unit="s", utc=True),
         end=pd.to_datetime(hourly.TimeEnd(), unit="s", utc=True),
         freq=pd.Timedelta(seconds=hourly.Interval()),
-        inclusive="left"
+        inclusive="right"
     )}
-    hourly_data["temperature_2m"] = hourly_temperature_2m
 
+    # Форматируем дату и время без временной зоны
+    hourly_data["Дата"] = hourly_data["Дата"].map(
+        lambda x: x.strftime("%Y-%m-%d %H:%M"))
+
+    hourly_data["Ростов-На-Дону"] = hourly_temperature_2m
+
+    # Создаем DataFrame
     hourly_dataframe = pd.DataFrame(data=hourly_data)
-    return hourly_dataframe.to_markdown(index=None)
+
+   # Форматируем числовые значения с одинаковым количеством отступов
+    hourly_dataframe = format_numbers(hourly_dataframe)
+
+    # Используем tabulate для красивого форматирования таблицы без индексов
+    table = tabulate(
+        hourly_dataframe,
+        headers="keys",
+        # tablefmt="pipe",   # Markdown стиль таблицы
+        tablefmt="grid",   # Используем формат grid для отображения в коде
+        # numalign="right",  # Выравнивание чисел по правому краю
+        stralign="left",   # Выравнивание строк по левому краю
+        showindex=False    # Отключаем индексы
+    )
+
+    return table
 
 
 def send_message(message):
@@ -67,4 +107,5 @@ def send_message(message):
 
 if __name__ == '__main__':
     weather = get_weather()
+    print(weather)
     send_message(weather)
